158d44d5c1121bd38edd4389cca8d85b4a10930c
Marcos Paulo <marcosp.mail@gmail.com>
2020-02-05 18:30:34 -0200

    chore: Create some controllers/middlewares and a seed

--- app/Models/Team.js
+++ app/Models/Team.js
@@ -4,10 +4,21 @@
 const Model = use('Model')
 
 class Team extends Model {
-    users () {
-        return this.belongsToMany('App/Models/User').pivotModel('App/Models/UserTeam')
-    }
+  static boot () {
+    super.boot()
 
+    this.addTrait('@provider:Lucid/Slugify', {
+      fields: {
+        slug: 'name'
+      },
+      strategy: 'dbIncrement',
+      disableUpdates: true
+    })
+  }
+
+  users () {
+    return this.belongsToMany('App/Models/User').pivotModel('App/Models/UserTeam')
+  }
 }
 
 module.exports = Team
